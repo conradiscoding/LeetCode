ad6d7c842bacb6f0fd474058a2c05ff0c44cd49c
radiscoding <cccooonnnrrraaaddd@gmail.com>
2024-04-11 22:10:29 -0400

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿
+    string s = "erase*****";
+    string sReturn = RemoveStars(s);
+    static string RemoveStars(string s)
+    {
+        while (s.Contains('*'))
+        {
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (s[i] == '*')
+                {
+                    s = s.Remove((i - 1), 2);
+                break;
+                }
+            }
+        }     
+            return s;
+    }
+
+Console.WriteLine(sReturn);
